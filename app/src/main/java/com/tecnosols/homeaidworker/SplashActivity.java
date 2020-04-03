9b5bfb8ba4f46dfd4b98a2ce9d46cf12d8b2c8e7
Deepak Singh Baise <deepaksinghbaise@gmail.com>
2020-04-03 21:49:11 +0530

    added splash activity successfully

--- app/src/main/java/com/tecnosols/homeaidworker/SplashActivity.java
+++ app/src/main/java/com/tecnosols/homeaidworker/SplashActivity.java
@@ -2,7 +2,12 @@ package com.tecnosols.homeaidworker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +15,38 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (user != null) {
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+
+                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                    //intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+                    startActivity(intent);
+                    finishAffinity();
+                }
+            }, 2 * 1000);
+
+        } else {
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+
+                    Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                    //intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finishAffinity();
+                }
+            }, 2 * 1000);
+
+        }
     }
 }
